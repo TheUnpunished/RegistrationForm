ee0ffc7fb9a24f5019397a7a412625c629a9502d
Skitel <skitelkin1999@gmail.com>
2018-10-15 13:16:45 +0300

    Initial Commit

---
+++ src/java4/FXMLDocumentController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java4;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author rbyhf
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private TextField TextField1;
+    @FXML
+    private TextField TextField2;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+//        System.out.println("You clicked me!");
+        String loginOf = TextField1.getText(), passOf = TextField2.getText();
+        user newUser = new user(loginOf,passOf);
+        boolean accepted = newUser.checkUser(loginOf, passOf);
+//        System.out.println(newUser.login);
+        if (accepted)
+        label.setText(newUser.getLogin() + " вошел");
+        else
+        label.setText("Неверные данные");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/java4/user.java
@@ -0,0 +1,60 @@
+
+package java4;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class user { 
+    private String login;
+    private String pass;
+
+    public user(String login, String pass) {
+        this.login = login;
+        this.pass = pass;
+    }
+    
+    boolean checkUser(String log, String pass)
+    {
+        try{
+            boolean findUser = false;
+            
+           
+            Scanner in = new Scanner(new File("file.txt"));
+            while(in.hasNextLine()) {
+                String[] strLine = in.nextLine().split(":");
+                
+//                for (int i = 0; i < strLine.length; i++){
+//                    System.out.println(strLine[0] + );
+//
+//                }
+//                System.out.println(strLine[1] + strLine[0].equals(log) + " " + strLine[1] + " " + strLine[1].equals(pass));
+                if (strLine[0].equals(log) && strLine[1].equals(pass)) {
+                    return true;
+                }
+            }
+            return findUser;
+        }catch (IOException e){
+            System.out.println("Ошибка");
+            return false;
+        }
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    
+}
