efa23144d5a1722b71e469ca343c1426c1c6c616
skitel <skitelkin1999@gmail.com>
2018-10-22 00:02:08 +0300

    Create title

--- src/java4/Java4.java
+++ src/java4/Java4.java
@@ -22,7 +22,7 @@ public class Java4 extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.setTitle("Вход");
         stage.setScene(scene);
         stage.show();
     }
