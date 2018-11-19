2f6f7dd538598cdb701c75437851685892e3687b
skitel <skitelkin1999@gmail.com>
2018-11-19 19:11:20 +0300

    added Purchases

--- src/java4/Purchases.java
+++ src/java4/Purchases.java
@@ -1,5 +1,15 @@
 package java4;
 
+import javax.xml.crypto.Data;
+import java.time.LocalDate;
+
 public class Purchases {
-    
+    Integer rowid;
+    String login;
+    LocalDate date;
+    listProduct products;
+
+    void getList(){
+        connection.getList();
+    }
 }
---
+++ src/java4/listProduct.java
@@ -0,0 +1,8 @@
+package java4;
+
+import java.util.List;
+
+public class listProduct {
+    List<product> list;
+    Integer numberOfOrder;
+}
